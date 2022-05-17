4a9556d6393b7affccb139d10736a07ffbc94e3c
Manuel Sahetapy <M_sahetapy@outlook.com>
2022-05-17 13:09:00 +0200

    added z rotation

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -8,6 +8,10 @@ function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
 
     myOtherBox.object3D.rotation.y -= rotationSpeed;
+
+    myOtherBox.object3D.rotation.z -= rotationSpeed;
+
+
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
